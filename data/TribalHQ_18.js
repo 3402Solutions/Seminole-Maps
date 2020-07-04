abc915cacf2c7ca586d423495dcc5b9a47233c2c
3402Solutions <66091895+3402Solutions@users.noreply.github.com>
2020-07-04 13:27:31 -0500

    Add files via upload

---
+++ data/TribalHQ_18.js
@@ -0,0 +1,8 @@
+var json_TribalHQ_18 = {
+"type": "FeatureCollection",
+"name": "TribalHQ_18",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "OBJECTID": "1", "label": "Seminole Nation of Oklahoma", "url": "www.seminolenation.com", "str_addres": "NE Jct Hwy US270 & St Hwy 56", "mail_addre": "P.O. Box 1498", "city_nm": "Wewoka", "state": "OK", "county": "Seminole", "zip_5": 74884.0, "phone_num": -242390096.0, "fax_num": -242390087.0, "contact_1": "Jennifer", "contact_2": "Johnson", "cont_title": "Media Specialist", "bia_agency": "Wewoka", "email_": "news@seminolenation.com" }, "geometry": { "type": "Point", "coordinates": [ -96.491075741686871, 35.128888105159604 ] } }
+]
+}
